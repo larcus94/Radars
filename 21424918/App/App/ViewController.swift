f1311ca8b1afb1438fb00f98daff50ab1b901185
Laurin Brandner <mail@laurinbrandner.ch>
2015-06-17 20:34:15 +0200

    Add 21424918

---
+++ 21424918/App/App/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  App
+//
+//  Created by Laurin Brandner on 17/06/15.
+//  Copyright © 2015 Laurin Brandner. All rights reserved.
+//
+
+import UIKit
+import Framework
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        Framework.helloTest()
+    }
+
+}
+
---
+++ 21424918/Framework/Framework/Test.swift
@@ -0,0 +1,13 @@
+//
+//  Test.swift
+//  Framework
+//
+//  Created by Laurin Brandner on 17/06/15.
+//  Copyright © 2015 Laurin Brandner. All rights reserved.
+//
+
+import Foundation
+
+public func helloTest() {
+    print("Hello Test")
+}
